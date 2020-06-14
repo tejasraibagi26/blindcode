c62804ed20d47cb56ebbe40bcfc8e5afd060f06f
tejasraibagi26 <tejasraibagi@gmail.com>
2020-06-14 18:14:18 +0530

    Updates

---
+++ precache-manifest.7880dc3a77819c95768dbef8bff210ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25e8449000bf1d2f6e4f082d422113a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9451d1f96c6db31898b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "38489494c2964d154bfd",
+    "url": "/static/css/main.ee831062.chunk.css"
+  },
+  {
+    "revision": "f9451d1f96c6db31898b",
+    "url": "/static/js/2.ddf28a2c.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/static/js/2.ddf28a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38489494c2964d154bfd",
+    "url": "/static/js/main.ca8d2610.chunk.js"
+  },
+  {
+    "revision": "23e035943ae4974af2a6",
+    "url": "/static/js/runtime-main.f86ef138.js"
+  }
+]);
